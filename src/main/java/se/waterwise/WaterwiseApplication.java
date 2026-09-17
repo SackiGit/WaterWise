63cfb9e4e3af07ad6b1fd6196bc2ffba89399f76
Oliver Sarac <oliversarac@gmail.com>
2026-09-17 14:13:57 +0200

    Set up initial Spring Boot project

---
+++ src/main/java/se/waterwise/WaterwiseApplication.java
@@ -0,0 +1,13 @@
+package se.waterwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterwiseApplication.class, args);
+	}
+
+}
