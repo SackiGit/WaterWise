691a1143459e37a8a5eae9e80770fd7234bb458b
oliversarac <oliversarac@gmail.com>
2026-09-23 10:38:19 +0200

    Initialize html template and added controller

---
+++ src/main/java/se/waterwise/controller/HomeController.java
@@ -0,0 +1,17 @@
+package se.waterwise.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "dashboard";
+    }
+}
